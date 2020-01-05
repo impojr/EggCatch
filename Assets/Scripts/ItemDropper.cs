e982917b375605716df1e0689a40677656d0941a
impoimpoimpo <aimpo@outlook.com>
2020-01-05 23:17:11 +1030

    Item Drop Refactoring

--- Assets/Scripts/ItemDropper.cs
+++ Assets/Scripts/ItemDropper.cs
@@ -26,6 +26,7 @@ public class ItemDropper : MonoBehaviour
 
     private int linesOfItemsDropped = 0;
     private float currentDropInterval;
+    private List<Transform> dropPositions;
 
     // Start is called before the first frame update
     void Start()
@@ -46,55 +47,13 @@ public class ItemDropper : MonoBehaviour
         {
             yield return new WaitForSeconds(currentDropInterval);
 
-            List<Transform> dropPositions = CreateDropLocationList();
+            dropPositions = CreateDropLocationList();
 
-            int positionToDropIndex = Random.Range(0, dropPositions.Count);
-            Transform positionToDrop = dropPositions[positionToDropIndex];
-
-            dropPositions.RemoveAt(positionToDropIndex);
-
-            GameObject itemToDrop = GetEggTypeToDrop();
-
-            GameObject item = Instantiate(itemToDrop, positionToDrop.position, Quaternion.identity, positionToDrop);
-
-            //second bomb logic
-            if (linesOfItemsDropped >= 15)
-            {
-                if (Random.Range(0, chanceOfFirstBomb) == 0)
-                {
-                    positionToDropIndex = Random.Range(0, dropPositions.Count);
-                    positionToDrop = dropPositions[positionToDropIndex];
-                    dropPositions.RemoveAt(positionToDropIndex);
-
-                    GameObject item2 = Instantiate(bomb, positionToDrop.position, Quaternion.identity, positionToDrop);
-                }
-
-                if (linesOfItemsDropped % 20 == 0)
-                {
-                    chanceOfFirstBomb = chanceOfFirstBomb == 1 ? 1 : chanceOfFirstBomb - 1;
-                }
-            }
-
-            //third bomb logic
-            if (linesOfItemsDropped >= 50)
-            {
-                if (Random.Range(0, chanceOfSecondBomb) == 0)
-                {
-                    positionToDropIndex = Random.Range(0, dropPositions.Count);
-                    positionToDrop = dropPositions[positionToDropIndex];
-                    dropPositions.RemoveAt(positionToDropIndex);
-
-                    GameObject item2 = Instantiate(bomb, positionToDrop.position, Quaternion.identity, positionToDrop);
-                }
-
-                if (linesOfItemsDropped % 20 == 0)
-                {
-                    chanceOfSecondBomb = chanceOfSecondBomb == 2 ? 2 : chanceOfSecondBomb - 1;
-                }
-            }
+            DropEgg();
+            DropBomb(15, true);
+            DropBomb(50, false);
 
             linesOfItemsDropped++;
-
             UpdateDropInterval();
         }
     }
@@ -111,12 +70,59 @@ public class ItemDropper : MonoBehaviour
         return dropPositions;
     }
 
+    private void DropEgg()
+    {
+        int positionToDropIndex = Random.Range(0, dropPositions.Count);
+        Transform positionToDrop = dropPositions[positionToDropIndex];
+
+        dropPositions.RemoveAt(positionToDropIndex);
+
+        GameObject eggToDrop = GetEggTypeToDrop();
+        GameObject eggItem = Instantiate(eggToDrop, positionToDrop.position, Quaternion.identity, positionToDrop);
+    }
+
     private GameObject GetEggTypeToDrop()
     {
         if (linesOfItemsDropped == 0)
             return egg;
 
         return linesOfItemsDropped % 10 == 0 ? goldenEgg : egg;
+    }
+
+    private void DropBomb(int lineItemsDropped, bool firstBomb)
+    {
+        int chanceOfBomb;
+
+        if (firstBomb)
+        {
+            chanceOfBomb = chanceOfFirstBomb;
+        } else
+        {
+            chanceOfBomb = chanceOfSecondBomb;
+        }
+
+        if (linesOfItemsDropped >= lineItemsDropped)
+        {
+            if (Random.Range(0, chanceOfBomb) == 0)
+            {
+                int positionToDropIndex = Random.Range(0, dropPositions.Count);
+                Transform positionToDrop = dropPositions[positionToDropIndex];
+                dropPositions.RemoveAt(positionToDropIndex);
+
+                GameObject bombItem = Instantiate(bomb, positionToDrop.position, Quaternion.identity, positionToDrop);
+            }
+
+            if (linesOfItemsDropped % 20 == 0)
+            {
+                if (firstBomb)
+                {
+                    chanceOfFirstBomb = chanceOfFirstBomb == 1 ? 1 : chanceOfFirstBomb - 1;
+                } else
+                {
+                    chanceOfSecondBomb = chanceOfSecondBomb == 2 ? 2 : chanceOfSecondBomb - 1;
+                }
+            }
+        }
     }
 
     private void UpdateDropInterval()
